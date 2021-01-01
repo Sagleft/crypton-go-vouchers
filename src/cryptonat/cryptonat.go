c9a49a7a86052b2a7a7ea58d157ce4158e873ee4
Sagleft <sagleft@inbox.ru>
2021-01-01 19:05:29 +0400

    add func placeholders

--- src/cryptonat/cryptonat.go
+++ src/cryptonat/cryptonat.go
@@ -25,3 +25,40 @@ type ActivationData struct {
 type Handler struct {
 	Client *utopiago.UtopiaClient
 }
+
+//SetClient - connects another Utopia client to Handler
+func (h *Handler) SetClient(client *utopiago.UtopiaClient) error {
+	//TODO: check client connection
+	h.Client = client
+	return nil
+}
+
+//ActivateVoucher - an attempt to activate a voucher and get data about it
+func (h *Handler) ActivateVoucher(voucherCode string) (ActivationData, error) {
+	//TODO
+	return ActivationData{}, nil
+}
+
+//CheckVoucherStatus - checks the voucher data, its activation status
+func (h *Handler) CheckVoucherStatus(referenceNumber int) (VoucherData, error) {
+	//TODO
+	return VoucherData{}, nil
+}
+
+//GetVoucherAmount - asks for the amount of the voucher if it has already been activated
+func (h *Handler) GetVoucherAmount(referenceNumber int) (float64, error) {
+	//TOOD
+	return 0, nil
+}
+
+//CreateVoucher - an attempt to create a voucher for a given amount
+func (h *Handler) CreateVoucher(float64) error {
+	//TODO
+	return nil
+}
+
+//GetNetFee - asks for a commission to create a voucher with a specific amount
+func (h *Handler) GetNetFee(amount float64) (float64, error) {
+	//TODO: user data from client
+	return amount * 0.0015, nil
+}
